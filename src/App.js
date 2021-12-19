1b90575ccc811ce4fc5bb0c93b44b9266bbceff6
Vedant Agarwal <vedanthello@gmail.com>
2021-12-19 20:32:36 +0530

    Get the JSON data from API
    Also,
    - Use UK flag as favicon
    - Remove unnecessary files and code that comes with create-react-app
    - Write README

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [APIResponseData, setAPIResponseData] = useState(null);
+
+  useEffect(() => {
+
+    axios.get("https://www.gov.uk/bank-holidays.json")
+      .then(res => {
+        setAPIResponseData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
